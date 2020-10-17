5f536709f69652bec740368877670b4f465b4356
Fati Pereira <53302048+GoldWomenAcademy@users.noreply.github.com>
2020-10-17 17:39:41 +0200

    add comment, post, user .js files

---
+++ backend/routes/comment.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const commentCtrl = require('../controllers/comment');
+
+//Routes Comment
+router.post('/:id', auth, commentCtrl.createComment);
+router.get('/:id', auth, commentCtrl.getAllComments);
+router.delete('/:id', auth, commentCtrl.deleteComment);
+
+module.exports = router;
---
+++ backend/routes/user.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const userCtrl = require('../controllers/user');
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+ 
+//Routes User
+router.post('/signup', multer, userCtrl.signup);
+router.post('/login', userCtrl.login);
+router.get('/', auth, userCtrl.getProfil);
+router.get('/admin', auth, userCtrl.getAllProfils);
+router.put('/', auth, multer, userCtrl.updateProfil);
+router.delete('/',auth, userCtrl.deleteProfil);
+
+module.exports = router;
+
